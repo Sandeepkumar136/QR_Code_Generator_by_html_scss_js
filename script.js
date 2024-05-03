70f4f6b939bb2de2510093426464897d4ff72499
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-05-03 23:59:39 +0530

    Finalizing Scripts

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const input=document.getElementById("input_qr");
 const qc_code=document.getElementById("img_qr");
 
 
-button.addEventListener('click', ()=>{
-    if(input=' '){
+button.addEventListener('click', function() {
+    if(input.value===''){
         alert("Please enter URL");
     }else{
-        input.src=`    qc_code=src='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=google.com/${input.value}
+        qc_code.src =`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=google.com/${input.value}
+        `
+        qc_code.alt =`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=google.com/${input.value}
         `
     }
 })
